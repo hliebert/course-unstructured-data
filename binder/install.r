583fd069e5c2aa71957e92fa88a963088986483c
Helge Liebert <helge.liebert@gmail.com>
2020-02-28 16:58:19 -0500

    First commit for binder environment.

---
+++ binder/install.r
@@ -0,0 +1,16 @@
+install.packages(
+  c(
+    "jsonlite",
+    "httr",
+    "rvest",
+    "RSelenium",
+    "wdman",
+    "stringr",
+    "glmnet",
+    "tidyverse",
+    "data.table",
+    "topicmodels",
+    "IRKernel"
+  ),
+  dependencies = TRUE
+)
