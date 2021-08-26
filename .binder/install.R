ac68966f23921ba4b15e6fd53b8140818b1b05ba
Helge Liebert <helge.liebert@gmail.com>
2021-08-26 17:52:52 +0200

    Test combining conda and install.r
    Using conda is much faster, but conda-forge does not have all libraries.

---
+++ .binder/install.R
@@ -0,0 +1,54 @@
+## install.packages(
+##   c(
+##     "twfy",
+##     "jsonlite",
+##     "xml2",
+##     "httr",
+##     "rvest",
+##     "RSelenium",
+##     "wdman",
+##     "webdriver",
+##     "tm",
+##     "quanteda",
+##     "spacyr",
+##     "readtext",
+##     "tidyverse",
+##     "tidytext",
+##     "wordcloud",
+##     "SentimentAnalysis",
+##     "naivebayes",
+##     "h2o",
+##     "fastNaiveBayes",
+##     "slam",
+##     "lsa",
+##     "glmnet",
+##     "lexicon",
+##     "qdap",
+##     "textclean",
+##     "text2vec",
+##     "uwot",
+##     "udpipe",
+##     "ggrepel",
+##     "factoextra",
+##     "fpc",
+##     "caret",
+##     "RCurl",
+##     "data.table",
+##     "IRkernel",
+##     "corpustools",
+##     "plotly",
+##     "plot3D",
+##     "devtools",
+##     "ranger",
+##     "reticulate",
+##     "Rcpp",
+##     "gtools",
+##     "ada"
+##   ),
+##   dependencies = TRUE
+## )
+
+install.packages("SentimentAnalysis")
+devtools::install_github("bnosac/word2vec", dependencies = TRUE)
+devtools::install_github("bnosac/doc2vec", dependencies = TRUE)
+devtools::install_github("bnosac/golgotha", INSTALL_opts = "--no-multiarch")
