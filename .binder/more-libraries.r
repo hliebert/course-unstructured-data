e7a95882a76d82f300bae1ab4a99637823be947c
Helge Liebert <helge.liebert@gmail.com>
2021-08-30 09:40:45 +0200

    Move to conda R packages.
    Clean apt file next.

---
+++ .binder/more-libraries.r
@@ -0,0 +1,4 @@
+devtools::install_github("bnosac/udpipe")
+devtools::install_github("bnosac/word2vec")
+devtools::install_github("bnosac/doc2vec")
+devtools::install_github("bnosac/golgotha", INSTALL_opts = "--no-multiarch")
